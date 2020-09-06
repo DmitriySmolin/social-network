474a52d00773cb7d142f822b63cad2fe8e453fec
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-09-06 16:27:59 +0400

    feat: add solution for lesson 60 (withRouter, props.match.params)

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -5,15 +5,18 @@ import { RootStateType, ProfileType } from "../../redux/redux-store";
 import { connect } from "react-redux";
 import { setUserProfileAC } from "../../redux/profile-reducer";
 import axios from "axios";
+import { withRouter } from "react-router-dom";
 
 type ProfileContainerType = {
   profile: ProfileType | null;
   setUserProfile: (profile: ProfileType) => void;
 };
 
-class ProfileContainer extends React.Component<ProfileContainerType> {
+class ProfileContainer extends React.Component<any> {
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then((res) => {
+    let userId = this.props.match.params.userId;
+    if (!userId) userId = 2;
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then((res) => {
       this.props.setUserProfile(res.data);
     });
   }
@@ -26,6 +29,8 @@ const mapStateToProps = (state: RootStateType) => {
   return { profile: state.profilePage.profile };
 };
 
+const WithUrlDataContainer = withRouter(ProfileContainer);
+
 export default connect(mapStateToProps, {
   setUserProfile: setUserProfileAC,
-})(ProfileContainer);
+})(WithUrlDataContainer);
